a097fa724374793b429eb57b2162aa27753e823a
musekorea <musekorea@hanmail.net>
2022-03-17 17:23:05 +0800

    map

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	Dimensions,
+	ActivityIndicator,
+} from "react-native";
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
 const ScreenWidth = Dimensions.get("window").width;
 const ScreenHeight = Dimensions.get("window").height;
+const Weather_API_Key = "08534ac244fcfd168c6f362552431a93";
 Location.setGoogleApiKey("AIzaSyAaA1obeZ7VuYSXkUn5MDYqruv9gAvTTHs");
 
 export default function App() {
 	const [locationOK, setLocationOK] = useState(true);
 	const [city, setCity] = useState("Loading...");
+	const [daily, setDaily] = useState([]);
 
 	const askLocationPermission = async () => {
 		try {
@@ -29,8 +38,13 @@ export default function App() {
 				},
 				{ useGoogleMaps: false }
 			);
-			console.log(locationData[0].city);
 			setCity(locationData[0].city);
+
+			const getWeather = await fetch(
+				`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${Weather_API_Key}&units=metric`
+			);
+			const jsonWeather = await getWeather.json();
+			setDaily(jsonWeather.daily);
 		} catch (error) {
 			console.log(error);
 		}
@@ -49,23 +63,33 @@ export default function App() {
 				showsHorizontalScrollIndicator={false}
 				contentContainerStyle={styles.weather}
 			>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27º</Text>
-					<Text style={styles.desc}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27º</Text>
-					<Text style={styles.desc}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27º</Text>
-					<Text style={styles.desc}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27º</Text>
-					<Text style={styles.desc}>Sunny</Text>
-				</View>
+				{daily.length === 0 ? (
+					<View style={styles.day}>
+						<ActivityIndicator
+							size="large"
+							color="white"
+							style={styles.activity}
+						/>
+					</View>
+				) : (
+					daily.map((day, index) => {
+						const unixTime = day.dt;
+						const date = new Date(unixTime * 1000).getDate();
+						let temp = day.temp.day;
+						temp = parseFloat(temp).toFixed(1);
+						return (
+							<View key={index} style={styles.day}>
+								<Text style={styles.temp}>{temp}º</Text>
+								<Text style={styles.desc}>{day.weather[0].description}</Text>
+								<Text style={styles.desc}>{date}日</Text>
+							</View>
+						);
+					})
+				)}
 			</ScrollView>
+			<View style={styles.footer}>
+				<Text style={styles.footer_text}>CODE ME.,CO.LTD</Text>
+			</View>
 		</View>
 	);
 }
@@ -76,13 +100,23 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
+		borderColor: "black",
+		borderBottomWidth: 2,
 	},
-	cityName: { fontSize: 40, fontWeight: "600" },
-	weather: { backgroundColor: "teal" },
+	cityName: { fontSize: 60, fontWeight: "600" },
+	weather: {},
 	day: {
 		width: ScreenWidth,
 		alignItems: "center",
 	},
 	temp: { fontSize: 120, marginTop: 50 },
-	desc: { fontSize: 50, marginTop: -20, position: "relative", left: -20 },
+	desc: { fontSize: 40, marginTop: 10, position: "relative", left: -20 },
+	activity: { marginTop: 100 },
+	footer: {
+		width: ScreenWidth,
+		position: "absolute",
+		bottom: 0,
+		marginBottom: 10,
+	},
+	footer_text: { fontSize: 20, textAlign: "right" },
 });
