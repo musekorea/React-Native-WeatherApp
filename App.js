00eba1139c7e5cb13ec276e56b5b82c1f7027b65
musekorea <musekorea@hanmail.net>
2022-03-19 13:13:43 +0800

    finish

--- App.js
+++ App.js
@@ -7,11 +7,23 @@ import {
 	ActivityIndicator,
 } from "react-native";
 import * as Location from "expo-location";
+import { Ionicons, Fontisto } from "@expo/vector-icons";
 import React, { useEffect, useState } from "react";
+
 const ScreenWidth = Dimensions.get("window").width;
 //const ScreenHeight = Dimensions.get("window").height;
 const Weather_API_Key = "08534ac244fcfd168c6f362552431a93";
 Location.setGoogleApiKey("AIzaSyAaA1obeZ7VuYSXkUn5MDYqruv9gAvTTHs");
+
+const icons = {
+	Thunderstorm: "lightnings",
+	Drizzle: "rain",
+	Rain: "rains",
+	Snow: "snow",
+	Atmosphere: "fog",
+	Clear: "day-sunny",
+	Clouds: "day-cloudy",
+};
 
 export default function App() {
 	const [locationOK, setLocationOK] = useState(true); //사용하지는 않았음
@@ -38,7 +50,6 @@ export default function App() {
 				},
 				{ useGoogleMaps: false }
 			);
-			console.log(locationData);
 			setCity(locationData[0].city);
 
 			const getWeather = await fetch(
@@ -80,8 +91,16 @@ export default function App() {
 						const date = new Date(unixTime * 1000).toString().substring(0, 10);
 						return (
 							<View key={index} style={styles.day}>
-								<Text style={styles.temp}>{temp}º</Text>
-								<Text style={styles.desc}>{day.weather[0].description}</Text>
+								<View style={styles.tempWrapper}>
+									<Text style={styles.temp}>{temp}º</Text>
+									<Fontisto
+										style={styles.icons}
+										name={icons[day.weather[0].main]}
+										size={80}
+										color="black"
+									></Fontisto>
+								</View>
+								<Text style={styles.desc}>{day.weather[0].main}</Text>
 								<Text style={styles.desc}>{date}</Text>
 							</View>
 						);
@@ -110,7 +129,14 @@ const styles = StyleSheet.create({
 		width: ScreenWidth,
 		alignItems: "center",
 	},
+	tempWrapper: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		width: "100%",
+	},
 	temp: { fontSize: 120, marginTop: 50 },
+	icons: { marginTop: 30, marginLeft: 30 },
 	desc: { fontSize: 40, marginTop: 10, position: "relative", left: -20 },
 	activity: { marginTop: 100 },
 	footer: {
